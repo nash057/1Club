0ac16a19d8672b29fbae44b4948e070033ef5606
Nihal Raj <nihalraj2000@gmail.com>
2022-05-12 23:37:28 +0530

    next commit

--- components/Partners.js
+++ components/Partners.js
@@ -4,17 +4,17 @@ import BDown from '../Images/BorderDown.png'
 
 function Partners() {
   return (
-    <div className="m-0 pb-0 pt-0 h-80 md:mt-60 text-white text-lg pr-0 bg-gradient-to-r from-green-700 via-green-400 to-green-900 justify-items-center">
-        <div className="resize-x">
+    <div className="flex-col m-0 pb-0 pt-0 h-80 md:mt-60 text-white text-lg pr-0 bg-gradient-to-r from-green-700 via-green-400 to-green-900 justify-items-center">
+        <div className="items-center">
           <Image 
-          className="mt-0 h-1 w-full block ml-auto mr-auto"
+          className="items-center mt-0 h-1 w-full justify-items-stretch "
           src={BUp}
           // layout="intrinsic"
           />
         </div>
         <p>Our Partners</p>
         <Image 
-        className="mb-0 h-1 w-full justify-items-stretch"
+        className="items-center mb-0 h-1 w-full justify-items-stretch"
         src={BDown}
         // height={60}
         />
