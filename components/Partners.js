9bbfcedddfb58a9e52c1d83cfd8336996618b494
Nihal Raj <nihalraj2000@gmail.com>
2022-05-12 19:56:40 +0530

    commit 3

--- components/Partners.js
+++ components/Partners.js
@@ -1,7 +1,23 @@
+import Image from 'next/image'
+import BUp from '../Images/BorderUp.png'
+import BDown from '../Images/BorderDown.png'
+
 function Partners() {
   return (
-    <div className="m-0 sm:h-32 md:h-64 md:mt-60 text-white text-lg p-10 bg-gradient-to-r from-green-700 via-green-400 to-green-900">
+    <div className="m-0 pb-0 pt-0 h-80 md:mt-60 text-white text-lg pr-0 bg-gradient-to-r from-green-700 via-green-400 to-green-900 justify-items-center">
+        <div className="resize-x">
+          <Image 
+          className="mt-0 h-1 w-full block ml-auto mr-auto"
+          src={BUp}
+          // layout="intrinsic"
+          />
+        </div>
         <p>Our Partners</p>
+        <Image 
+        className="mb-0 h-1 w-full justify-items-stretch"
+        src={BDown}
+        // height={60}
+        />
     </div>
   )
 }
