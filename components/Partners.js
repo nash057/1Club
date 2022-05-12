d35e323596fa7bdf8c2f9a5eee57c969e7b1db38
Nihal Raj <nihalraj2000@gmail.com>
2022-05-12 23:41:31 +0530

    commit3

--- components/Partners.js
+++ components/Partners.js
@@ -7,14 +7,14 @@ function Partners() {
     <div className="flex-col m-0 pb-0 pt-0 h-80 md:mt-60 text-white text-lg pr-0 bg-gradient-to-r from-green-700 via-green-400 to-green-900 justify-items-center">
         <div className="items-center">
           <Image 
-          className="items-center mt-0 h-1 w-full justify-items-stretch "
+          className="items-center block mt-0 h-1 w-full justify-items-stretch "
           src={BUp}
           // layout="intrinsic"
           />
         </div>
         <p>Our Partners</p>
         <Image 
-        className="items-center mb-0 h-1 w-full justify-items-stretch"
+        className="items-center block mb-0 h-1 w-full justify-items-stretch"
         src={BDown}
         // height={60}
         />
