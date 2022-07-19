a00888f4a797ff40f52780097dd24ec3448eff29
Noisycall <rajekunal4@gmail.com>
2022-07-19 17:43:23 +0530

    Changed loader for next/image

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import Image from "next/dist/client/image"
+import Image from "./CustomLoader"
 import Logo from "../Images/ClubLogo.jpeg"
 import {
     WhatsappShareButton,
@@ -12,10 +12,10 @@ import DiscordLogo from '../Images/discord1.png'
 function Header() {
   return (
     <div className="flex flex-col top-0 sm:flex-row md:ml-20 justify-between items-center md:mr-2 h-auto px-4 py-2 rounded-md">
-        <Image 
+        <Image
             className="object-cover rounded-2xl mt-1"
-            src={Logo} 
-            width={200} 
+            src={Logo}
+            width={200}
             height={75}
             alt=""
         />
@@ -28,13 +28,13 @@ function Header() {
         </WhatsappShareButton> */}
 
         <div className="inline-flex sm:flex-wrap">
-            {/* <span>Join us on WhatsApp</span> */}        
+            {/* <span>Join us on WhatsApp</span> */}
             <a className="p-2 sm:hover:scale-105 hover:z-50" href="https://discord.com/invite/CjCS2vseUD">
-                <Image 
+                <Image
                     className="object-cover rounded-2xl"
-                    src={DiscordLogo} 
-                    width={60} 
-                    height={60} 
+                    src={DiscordLogo}
+                    width={60}
+                    height={60}
                     alt=""
                 />
             </a>
@@ -44,10 +44,10 @@ function Header() {
             <a className="p-2 sm:hover:scale-105 hover:z-50" href="https://chat.whatsapp.com/C2UEC7NfSQt6SxBpJxt3XA">
                 <WhatsappIcon className="WhatsApp" size={'3.75rem'} borderRadius={20} round={false}/>
             </a>
-            
+
         </div>
-        
-            
+
+
     </div>
   )
 }
--- components/Partners.js
+++ components/Partners.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "./CustomLoader"
 import BUp from '../Images/BorderUp.png'
 import BDown from '../Images/BorderDown.png'
 
@@ -6,7 +6,7 @@ function Partners() {
   return (
     <div className="flex-col m-0 py-0 pt-0 h-80 md:mt-60 text-white text-lg pr-0 bg-gradient-to-r from-green-700 via-green-400 to-green-900 justify-items-center">
         <div className="items-center md:hidden">
-          <Image 
+          <Image
           className="items-center mt-0 h-1 w-full justify-items-stretch"
           src={BUp}
           // layout="intrinsic"
@@ -14,7 +14,7 @@ function Partners() {
         </div>
         <p>Our Partners</p>
         <div className='items-ceter md:hidden'>
-          <Image 
+          <Image
             className="items-center mb-0 my-0 h-1 w-full justify-items-stretch"
             src={BDown}
           />
